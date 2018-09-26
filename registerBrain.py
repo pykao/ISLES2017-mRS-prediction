fe3c3615d8246e004f9aa830dfabe3c0b8f7208b
Po-Yu Kao <poyu_kao@ucsb.edu>
2018-09-25 17:47:32 -0700

    added RegisterBrain.py

--- registerBrain.py
+++ registerBrain.py
@@ -1,3 +1,13 @@
 import paths
 import os
-from subprocessing import Pool
+
+
+def ISLES2017TrainingADCPaths(isles_train_dir=paths.isles2017_training_dir):
+	adc_filepaths = [os.path.join(root, name) for root, dirs, files in os.walk(isles_train_dir) 
+	for name in files if 'MR_ADC' in name and name.endswith('.nii')]
+	adc_filepaths.sort()
+	return adc_filepaths
+
+adc_filepaths = ISLES2017TrainingADCPaths()
+
+print(len(adc_filepaths))
