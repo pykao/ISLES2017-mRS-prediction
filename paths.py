fdcec371245e172619d9c9733b229a1e34a5dbb9
Po-Yu Kao <poyu_kao@ucsb.edu>
2018-09-25 17:47:04 -0700

    added two more paths

--- paths.py
+++ paths.py
@@ -2,4 +2,9 @@ isles2017_training_dir = "/media/pkao/Dataset/ISLES2017/train"
 
 isles2017_testing_dir = "/media/pkao/Dataset/ISLES2017/test"
 
+# The directory of the MNI152 T1 1mm atlas
+mni152_1mm_path = '/usr/share/fsl/data/standard/MNI152_T1_1mm_brain.nii.gz'
+# The directory of dsi studio
+dsi_studio_path = '/home/pkao/dsistudio/dsi_studio_64'
+
 
