8cc2946cc626b3b8634fcff4dd97f38200a8a80f
Po-Yu Kao <poyu_kao@ucsb.edu>
2018-10-17 21:34:09 -0700

    roa fiber

--- gt_fiber.py
+++ gt_fiber.py
@@ -19,7 +19,7 @@ dst_dir = os.path.join(work_dir, 'gt_stroke')
 if not os.path.exists(dst_dir):
     os.mkdir(dst_dir)
 
-MoveLesionsMNIMask(paths.isles2017_training_dir, dst_dir)
+#MoveLesionsMNIMask(paths.isles2017_training_dir, dst_dir)
 
 network_measures_dir = os.path.join(work_dir, 'network_measures')
 
@@ -58,19 +58,20 @@ stroke_files_dir = os.listdir(dst_dir)
 stroke_files_dir.sort()
 assert(len(stroke_files_dir)==43)
 
+region_prop ='--roa='
+
 # pass type of connectivity matrices
 for idx, stroke_file in enumerate(stroke_files_dir):
 
     pat_name = stroke_file[:stroke_file.find('_MNI152_T1_1mm.nii.gz')]
     print(idx, pat_name)
-    roi = '--roi='+os.path.join(stroke_dir, stroke_file)
-    print(roi)
+    #roi = '--roi='+os.path.join(stroke_dir, stroke_file)
     #seed = '--seed='+os.path.join(stroke_dir, stroke_file)
     connectivity_type = '--connectivity_type=pass'
     connectivity_value = '--connectivity_value=count'
     connectivity_threshold = '--connectivity_threshold=0'
     #subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, seed, roi, parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
-    subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, roi, parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
+    subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, region_prop+os.path.join(stroke_dir, stroke_file), parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
 
     network_measure_files = [os.path.join(root, name) for root, dirs, files in os.walk(work_dir) for name in files if 'network_measures' in name and name.endswith('.txt')]
     network_measure_file_dst = os.path.join(os.path.split(network_measure_files[0])[0], 'network_measures', 'gt_stroke', os.path.split(network_measure_files[0].replace(source, pat_name))[1])
@@ -89,14 +90,13 @@ for idx, stroke_file in enumerate(stroke_files_dir):
 
     pat_name = stroke_file[:stroke_file.find('_MNI152_T1_1mm.nii.gz')]
     print(idx, pat_name)
-    roi = '--roi='+os.path.join(stroke_dir, stroke_file)
-    print(roi)
+    #roi = '--roi='+os.path.join(stroke_dir, stroke_file)
     #seed = '--seed='+os.path.join(stroke_dir, stroke_file)
     connectivity_type = '--connectivity_type=end'
     connectivity_value = '--connectivity_value=count'
     connectivity_threshold = '--connectivity_threshold=0'
     #subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, seed, roi, parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
-    subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, roi, parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
+    subprocess.call(['./dsi_studio', '--action=trk', '--source='+source, region_prop+os.path.join(stroke_dir, stroke_file), parameter_id, '--output=no_file', '--connectivity=aal', connectivity_type, connectivity_value, connectivity_threshold])
 
     network_measure_files = [os.path.join(root, name) for root, dirs, files in os.walk(work_dir) for name in files if 'network_measures' in name and name.endswith('.txt')]
     network_measure_file_dst = os.path.join(os.path.split(network_measure_files[0])[0], 'network_measures', 'gt_stroke', os.path.split(network_measure_files[0].replace(source, pat_name))[1])
--- predict.py
+++ predict.py
@@ -130,15 +130,15 @@ y_pred_label = np.zeros((37,1), dtype=int)
 
 logging.info('RFECV Feature selection...')
 # rfecv 
-#estimator = LogisticRegression(penalty='l2', class_weight='balanced', random_state=0, multi_class='multinomial', solver='lbfgs', n_jobs=-1)
-estimator = RandomForestRegressor(n_estimators=300, criterion='mse', random_state=0, n_jobs=2)
+estimator = LogisticRegression(penalty='l2', class_weight='balanced', random_state=0, multi_class='multinomial', solver='lbfgs', n_jobs=-1)
+#estimator = RandomForestRegressor(n_estimators=300, criterion='mse', random_state=0, n_jobs=2)
 #estimator = RandomForestClassifier(n_estimators=100, n_jobs=-1)
 #rfecv = RFECV(estimator, step=1, cv=loo, scoring='accuracy', n_jobs = -1)
 rfecv = RFECV(estimator, step=1, cv=loo, scoring='neg_mean_absolute_error', n_jobs = 2)
 rfecv.fit(X, y)
 X_rfecv = rfecv.transform(X)
-#logging.info('Logistic Regression, Optimal number of features: %d' % X_rfecv.shape[1])
-logging.info('Random Forest Regressior, Optimal number of features: %d' % X_rfecv.shape[1])
+logging.info('Logistic Regression, Optimal number of features: %d' % X_rfecv.shape[1])
+#logging.info('Random Forest Regressior, Optimal number of features: %d' % X_rfecv.shape[1])
 
 
 logging.info('Predicting...')
@@ -156,19 +156,19 @@ for train_index, test_index in loo.split(X_rfecv):
 	#print(rfc.predict(X_test))
 	
 	# Random Forest Regressior
-	rfr = RandomForestRegressor(n_estimators=300, criterion='mse', random_state=0, n_jobs=2)
-	rfr.fit(X_train, y_train)
+	#rfr = RandomForestRegressor(n_estimators=300, criterion='mse', random_state=0, n_jobs=2)
+	#rfr.fit(X_train, y_train)
 	#accuracy[idx] = rfr.score(X_test, y_test)
-	y_pred_label[idx] = np.round(rfr.predict(X_test))
-	accuracy[idx] = accuracy_score(y_pred_label[idx], y_test)
+	#y_pred_label[idx] = np.round(rfr.predict(X_test))
+	#accuracy[idx] = accuracy_score(y_pred_label[idx], y_test)
 
-
-	#lr = LogisticRegression(penalty='l2', class_weight='balanced', random_state=0, multi_class='multinomial', solver='lbfgs', n_jobs=-1)
-	#lr.fit(X_train, y_train)
-	#accuracy[idx] = lr.score(X_test, y_test)
-	#y_pred_label[idx] = lr.predict(X_test)
+	# Logistic Regression
+	lr = LogisticRegression(penalty='l2', class_weight='balanced', random_state=0, multi_class='multinomial', solver='lbfgs', n_jobs=-1)
+	lr.fit(X_train, y_train)
+	accuracy[idx] = lr.score(X_test, y_test)
+	y_pred_label[idx] = lr.predict(X_test)
 	
 
 	idx += 1
 
-logging.info("Best Scores of features  - Using RFR - Accuracy: %0.4f (+/- %0.4f), MAE: %0.4f (+/- %0.4f)" %(np.mean(accuracy), np.std(accuracy), np.mean(np.absolute(y-y_pred_label)), np.std(np.absolute(y-y_pred_label))))
+logging.info("Best Scores of features  - Using LR - Accuracy: %0.4f (+/- %0.4f), MAE: %0.4f (+/- %0.4f)" %(np.mean(accuracy), np.std(accuracy), np.mean(np.absolute(y-y_pred_label)), np.std(np.absolute(y-y_pred_label))))
