ef7b9cde409a3f56257a30c07c531a3198914a76
Po-Yu Kao <poyu_kao@ucsb.edu>
2019-01-24 16:40:26 -0800

    add more details

--- paths.py
+++ paths.py
@@ -1,10 +1,8 @@
+# The path you store the clinical parameters file
 isles2017_dir = "/media/pkao/Dataset/ISLES2017"
-
+# The path you save ISLES2017 training data
 isles2017_training_dir = "/media/pkao/Dataset/ISLES2017/train"
-
 # The directory of the MNI152 T1 1mm atlas
 mni152_1mm_path = '/usr/share/fsl/data/standard/MNI152_T1_1mm_brain.nii.gz'
 # The directory of dsi studio
 dsi_studio_path = '/home/pkao/dsistudio/dsi_studio_64'
-
-
