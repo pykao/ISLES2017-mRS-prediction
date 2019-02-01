0e84bd7b17f4177e961b77b6a4c918767ef67ca6
Po-Yu Kao <poyu_kao@ucsb.edu>
2019-01-31 21:18:10 -0800

    updated

--- oskar_predict_loo.py
+++ oskar_predict_loo.py
@@ -5,8 +5,10 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import VarianceThreshold, RFECV
 from skimage.measure import regionprops, marching_cubes_classic, mesh_surface_area
+from utils import extract_gt_mRS
 
 y = np.load('./ISLES2017_gt.npy')
+
 oskar_features = np.load('./oskar_ISLES2016_features.npy')
 
 sel = VarianceThreshold(0.5*(1-0.5))
@@ -17,12 +19,12 @@ normalized_selected_oskar_features = scaler.fit_transform(selected_oskar_feature
 
 # Leave One Out Cross Validation
 loo = LeaveOneOut()
-estimator = RandomForestRegressor(n_estimators=300, max_depth=3, random_state=1989, n_jobs=-1)
-rfecv = RFECV(estimator, step=1, cv=loo, scoring='neg_mean_absolute_error', n_jobs=-1)
+#estimator = RandomForestRegressor(n_estimators=300, max_depth=3, random_state=1989, n_jobs=-1)
+#rfecv = RFECV(estimator, step=1, cv=loo, scoring='neg_mean_absolute_error', n_jobs=-1)
 
 
-X = rfecv.fit_transform(normalized_selected_oskar_features, y)
-#X = normalized_selected_all_features
+#X = rfecv.fit_transform(normalized_selected_oskar_features, y)
+X = normalized_selected_oskar_features
 #X = all_features
 
 y_pred_label = np.zeros((37,1), dtype=np.float32)
@@ -31,6 +33,7 @@ idx=0
 for train_index, test_index in loo.split(X):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
+    print(y_test)
     estimator = RandomForestRegressor(n_estimators=300, max_depth=3, random_state=1989, n_jobs=-1)
     estimator.fit(X_train, y_train)
     y_pred = estimator.predict(X_test)
