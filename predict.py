9880e69471293c5131d7303d75c41a2270bbd87f
Po-Yu Kao <poyu_kao@ucsb.edu>
2019-02-15 16:03:42 -0800

    changed the filename

--- predict.py
+++ predict.py
@@ -25,7 +25,7 @@ from utils import extract_volumetric_spatial_features, extract_morphological_fea
 from xgboost import XGBRegressor
 
 # =========================================== setup logs ====================================== #
-log = os.path.join(os.getcwd(), 'log_new.txt')
+log = os.path.join(os.getcwd(), 'log.txt')
 fmt = '%(asctime)s %(message)s'
 logging.basicConfig(level=logging.INFO, format=fmt, filename=log)
 console = logging.StreamHandler()
@@ -61,12 +61,12 @@ rfecv_feature_selection = True
 #feature_type = 'tract_nrm_pass'
 #feature_type = 'tract_bin_pass'
 #feature_type = 'tract_dsi_end'
-#feature_type = 'tract_nrm_end'
-feature_type = 'tract_bin_end'
+feature_type = 'tract_nrm_end'
+#feature_type = 'tract_bin_end'
 # original
-#weight_type = 'original'
+weight_type = 'original'
 # modified
-weight_type = 'modified'
+#weight_type = 'modified'
 # one
 #weight_type = 'one'
 
@@ -132,7 +132,7 @@ if not os.path.exists('./features/'):
 #np.save('./features/morphological_features.npy', features)
 #np.save('./features/ori_aal_features.npy', features)
 #np.save('./features/ori_tract_nrm_end_aal_features.npy', features)
-np.save('./features/mod_tract_bin_end_aal_features.npy', features)
+#np.save('./features/mod_tract_bin_end_aal_features.npy', features)
 
 # ==============================  Feature Normalization ========================================= #
 
@@ -217,7 +217,7 @@ if not os.path.exists('./predicted_labels/'):
 #np.save('./predicted_labels/rfecv_ori_tract_nrm_end_aal_pred_loo.npy', y_pred_label)
 #np.save('./predicted_labels/ori_tract_nrm_end_aal_pred_loo.npy', y_pred_label)
 #np.save('./predicted_labels/oskar_pred_loo.npy', y_pred_label)
-np.save('./predicted_labels/mod_tract_bin_end_aal_pred_loo.npy', y_pred_label)
+#np.save('./predicted_labels/mod_tract_bin_end_aal_pred_loo.npy', y_pred_label)
 
 # =========================== Feature Importance ================================ #
 
@@ -236,94 +236,47 @@ logging.info(feature_type+" features with RF Regressor - Accuracy: %0.2f , MAE: 
 #plt.bar(x_range, lesion_histogram)
 #plt.show()
 
-# AAL top 5 features
-#region_7_importances = subject_feature_importances[:,6]
-#region_14_importances = subject_feature_importances[:,13]
-#region_41_importances = subject_feature_importances[:,40]
-#region_65_importances = subject_feature_importances[:,64]
-#region_89_importances = subject_feature_importances[:,88]
-#regions_importances = [region_89_importances, region_7_importances, region_65_importances, region_41_importances, region_14_importances]
-#fig, ax = plt.subplots(1, 1)
-#ax.boxplot(regions_importances, showmeans=True)
-#ax.set_title('Region Importance', fontsize = 30)
-#plt.ylabel('Importance', fontsize = 20)
-#region_names = ['Left\ninferior temporal gyrus', 'Left\nmiddle frontal gyrus', 'Left\nangular gyrus', 'Left amygdala', 'Triangular part of\nright inferior frontal gyrus'] 
-#plt.xticks(np.arange(1,6), region_names,  fontsize = 20)
-#plt.yticks(fontsize = 20)
-#plt.show()
-
-# AAL selected features
-#region_16_importances = subject_feature_importances[:,0]
-#region_18_importances = subject_feature_importances[:,1]
-#region_41_importances = subject_feature_importances[:,2]
-#region_89_importances = subject_feature_importances[:,3]
-#regions_importances = [region_89_importances, region_18_importances, region_41_importances, region_16_importances]
-#fig, ax = plt.subplots(1, 1)
-#ax.boxplot(regions_importances, showmeans=True)
-#ax.set_title('Region Importance', fontsize = 30)
-##plt.xlabel('Region', fontsize = 20)
-#plt.ylabel('Importance', fontsize = 20)
-#region_names = ['Left \ninferior temporal gyrus', 'Right \nRolandic operculum', 'Left amygdala', 'Orbital part of \nright inferior frontal gyrus'] 
-#plt.xticks(np.arange(1,5), region_names,  fontsize = 20)
-#plt.yticks(fontsize = 20)
-#plt.show()
+'''# AAL top 7 features
+regions_importances = [subject_feature_importances[:,88],
+                       subject_feature_importances[:,6],
+                       subject_feature_importances[:,17],
+                       subject_feature_importances[:,72],
+                       subject_feature_importances[:,64],
+                       subject_feature_importances[:,15],
+                       subject_feature_importances[:,13]]
+fig, ax = plt.subplots(1, 1)
+ax.boxplot(regions_importances, showmeans=True)
+ax.set_title('Region Importance', fontsize = 60)
+plt.ylabel('Importance', fontsize = 40)
+region_names = ['LITG', 'LMFG', 'RRO', 'LP', 'LAG', 'ORIFG', 'TRIFG'] 
+plt.xticks(np.arange(1,8), region_names,  fontsize = 20)
+plt.yticks(fontsize = 20)
+plt.show()'''
 
 
-'''# ================================= Tractographic Features =================================================== #
 
-tractographic_feature_list = ['W_dis_pass', 'W_nrm_pass', 'W_bin_pass', 'W_dsi_end', 'W_nrm_end', 'W_bin_end']
-
-all_tractographic_features = np.stack((W_dsi_pass, W_nrm_pass, W_bin_pass, W_dsi_end, W_nrm_end, W_bin_end), axis=-1)
-
-for i in np.arange(6):
-    tractographic_feature = all_tractographic_features[:, :, i]
-    logging.info('Using Tractographic Features')
-    logging.info(tractographic_feature_list[i])
-    
-    logging.info('Features normalization...')
-    scaler = StandardScaler()
-    normalized_tractographic_feature = scaler.fit_transform(tractographic_feature)
-    logging.info('Completed features normalization...')
-    
-    logging.info('Remove features with low variance...')
-    sel = VarianceThreshold(0)
-    selected_normalized_tractographic_feature = sel.fit_transform(normalized_tractographic_feature)
-    
-    X = selected_normalized_tractographic_feature
-    y = mRS_gt
-
-    # Cross Validation Model
-    loo = LeaveOneOut()
-    
-    accuracy = np.zeros((37,1), dtype=np.float32)
-    y_pred_label = np.zeros((37,1), dtype=np.float32)
-    y_abs_error = np.zeros((37,1), dtype=np.float32)
-    
-    logging.info('RFECV Feature selection...')
-    # rfecv 
-    estimator = RandomForestRegressor(n_estimators=300, max_depth=3, random_state=1989, n_jobs=-1)
-    #estimator = RandomForestClassifier(n_estimators=300, max_depth=3, random_state=1989, n_jobs=-1, class_weight="balanced_subsample")
-    #rfecv = RFECV(estimator, step=1, cv=loo, scoring='neg_mean_squared_error', n_jobs=-1)
-    rfecv = RFECV(estimator, step=1, cv=loo, scoring='neg_mean_absolute_error', n_jobs=-1)
-    rfecv.fit(X, y)
-    X_rfecv = rfecv.transform(X)
-    logging.info('Random Forest Regressor, Optimal number of features: %d' % X_rfecv.shape[1])
-    #X_rfecv = normalized_tractographic_feature
-
-    logging.info('Predicting...')
-    idx = 0
-    for train_index, test_index in loo.split(X_rfecv):
-        X_train, X_test = X_rfecv[train_index], X_rfecv[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        # Random Forest Regressor
-        rfr = RandomForestRegressor(n_estimators=300, max_depth=3, random_state=1989, n_jobs=-1)
-        rfr.fit(X_train, y_train)
-        # Random Froest Classifier
-        #rfr = RandomForestClassifier(n_estimators=300, max_depth=3, random_state=1989, n_jobs=-1, class_weight="balanced_subsample")
-        #rfr.fit(X_train, y_train)
-        y_pred_label[idx] = np.round(rfr.predict(X_test))
-        accuracy[idx] = accuracy_score(np.round(rfr.predict(X_test)), y_test)
-        y_abs_error[idx] = np.absolute(y_pred_label[idx]-y_test)
-        idx += 1
-    logging.info("Best Scores of features - Using Random Forest Regressor - Accuracy: %0.4f , MAE: %0.4f (+/- %0.4f)" %(np.mean(accuracy), np.mean(y_abs_error), np.std(y_abs_error)))
-'''
+'''# AAL selected features
+regions_importances = [subject_feature_importances[:,6],
+                       subject_feature_importances[:,3],
+                       subject_feature_importances[:,0],
+                       subject_feature_importances[:,2],
+                       subject_feature_importances[:,1],
+                       subject_feature_importances[:,4],
+                       subject_feature_importances[:,5],
+                       subject_feature_importances[:,7]]
+fig, ax = plt.subplots(1, 1)
+ax.boxplot(regions_importances, showmeans=True)
+ax.set_title('Region Importance', fontsize = 60)
+#plt.xlabel('Region', fontsize = 20)
+plt.ylabel('Importance', fontsize = 40)
+region_names = ['LITG',
+                'RRO',
+                'LMFG',
+                'ORIFG',
+                'TRIFG',
+                'LAG',
+                'LP',                
+                'RITG'] 
+plt.xticks(np.arange(1,9), region_names,  fontsize = 30)
+plt.yticks(fontsize = 20)
+plt.show()'''
